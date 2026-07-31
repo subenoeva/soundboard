cfa422860bd8e2244195be0f6c0caed07125f131
Pablo <pcostaoubina@gmail.com>
2026-08-01 01:26:39 +0200

    chore(master): release 0.4.0 (#16)

--- src/soundboard/__init__.py
+++ src/soundboard/__init__.py
@@ -4,4 +4,4 @@
 # The self-updater reads this at runtime to decide whether a published release is newer,
 # so it has to be the real version: importlib.metadata is not an option because
 # PyInstaller onefile builds ship no dist-info.
-__version__ = "0.3.0"  # x-release-please-version
+__version__ = "0.4.0"  # x-release-please-version
